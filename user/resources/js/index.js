ca72604db2303837a8b65383c8f13baf3fd593e4
kaushikSarma <kausyap10@gmail.com>
2017-01-15 01:21:45 +0000

    chat UI improvement, chat backend integration

--- user/resources/js/index.js
+++ user/resources/js/index.js
@@ -2,36 +2,48 @@ var socket = io();
 
 $(document).ready(function(){
   $("#chatMessageSend").click(function(){
-    var msg = document.getElementById('chatMessageInput').value;
+    var msg = $('#chatMessageInput').val();
     $('#chatMessageInput').val('');
     if(msg){
-        socket.emit('msg', {message: msg, user: 'user'});
+      socket.emit('msg', {message: msg, user: 'user'});
     }
   });
-});
+  var user;
 
-var user;
+  socket.on('userExists',function(data){
+  //  $('#nickName').val('');
+  //  $("#usernameInput").css("border-bottom", "2px solid #f00");
+  });
 
-socket.on('userExists',function(data){
-//  $('#nickName').val('');
-//  $("#usernameInput").css("border-bottom", "2px solid #f00");
-});
+  socket.emit('setUserName', user);
 
-socket.emit('setUserName', user);
+  socket.on('userSet',function(data){
+    // $('#nickName').val('');
+    // user = data.userName;
+    // $(".login.page").css("display","none");
+    // $(".chat.page").css("display","block");
+    // flag = true;
+    // initialize();
+    // scroll();
+  });
 
-socket.on('userSet',function(data){
-  // $('#nickName').val('');
-  // user = data.userName;
-  // $(".login.page").css("display","none");
-  // $(".chat.page").css("display","block");
-  // flag = true;
-  // initialize();
-  // scroll();
-});
+  socket.on('newmsg', function(data){
+    if(user){
+    var item = $('<div class="timeslot" style="height: 94px;"> <div class="task"> <span> <span class="type"> ' + data.user + ' </span> <span class="details"> ' + data.message + ' </span><div class="arrow"></div> </div> <div class="icon green"> </div> <div class="time"> ' + data.time + ' </div> </div>');
+      $('.timeline').append(item);
+      scroll();
+    }
+  });
+});	
+function toTime(input){
+	var h, cycle, time;
+	h = input.substr(0, 2);
+	cycle = (h >= 12) ? 'pm' : 'am';
+	time = {
+		hr: h % 13 + 1,
+		min: input.substr(3, 2),
+		cycle:  cycle
+	};
+	return time;
+}
 
-socket.on('newmsg', function(data){
-  if(user){
-    $('.timeline').append('<div class="timeslot" style="height: 94px;"> <div class="task"> <span> <span class="type"> ' + data.user + ' </span> <span class="details"> ' + data.message + ' </span> </div> <div class="arrow"></div> </div> <div class="icon green"> </div> <div class="time"> ' + data.time + ' </div> </div>');
-    scroll();
-  }
-});
